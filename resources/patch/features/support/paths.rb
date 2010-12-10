2f70c2ba2b8da26a53d686ce6c290914b553083f
Mack Earnhardt <mack@agilereasoning.com>
2010-12-10 18:07:12 -0500

    Tweak cucumber setttings

--- resources/patch/features/support/paths.rb
+++ resources/patch/features/support/paths.rb
@@ -1,7 +1,16 @@
 diff --git a/features/support/paths.rb b/features/support/paths.rb
-index 06b3efb..353e04e 100644
+index 06b3efb..cdca584 100644
 --- a/features/support/paths.rb
 +++ b/features/support/paths.rb
+@@ -9,7 +9,7 @@ module NavigationHelpers
+     case page_name
+ 
+     when /the home\s?page/
+-      '/'
++      root_path
+ 
+     # Add more mappings here.
+     # Here is an example that pulls values out of the Regexp:
 @@ -17,6 +17,16 @@ module NavigationHelpers
      #   when /^(.*)'s profile page$/i
      #     user_profile_path(User.find_by_login($1))
