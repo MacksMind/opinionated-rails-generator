79b4d56bae0ee75c6f9d924584ddf716ddf48e8a
Mack Earnhardt <mack@agilereasoning.com>
2012-10-01 06:31:46 -0400

    Extract Heroku changes into separate patch file
    Doesn't include Gemfile changes

--- resources/patch/config/environments/production.rb
+++ resources/patch/config/environments/production.rb
@@ -2,14 +2,6 @@ diff --git a/config/environments/production.rb b/config/environments/production.
 index b5f82c4..f5e4587 100644
 --- a/config/environments/production.rb
 +++ b/config/environments/production.rb
-@@ -10,6 +10,7 @@ Shiny::Application.configure do
- 
-   # Disable Rails's static asset server (Apache or nginx will already do this)
-   config.serve_static_assets = false
-+  config.static_cache_control = "public, max-age=172800"
- 
-   # Compress JavaScripts and CSS
-   config.assets.compress = true
 @@ -28,7 +29,7 @@ Shiny::Application.configure do
    # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
  
@@ -19,10 +11,3 @@ index b5f82c4..f5e4587 100644
  
    # See everything in the log (default is :info)
    # config.log_level = :debug
-@@ -64,4 +65,6 @@ Shiny::Application.configure do
-   # Log the query plan for queries taking more than this (works
-   # with SQLite, MySQL, and PostgreSQL)
-   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-+
-+  config.middleware.use Rack::Deflater
- end
