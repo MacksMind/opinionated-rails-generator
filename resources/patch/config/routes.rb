737abef3d10415c793b7292ef1772899c7ac9e91
Mack Earnhardt <mack@springbuk.com>
2014-03-27 13:23:44 -0400

    Remove Agile Reasoning references
    Except the favicon

--- resources/patch/config/routes.rb
+++ resources/patch/config/routes.rb
@@ -2,7 +2,7 @@ diff --git a/config/routes.rb b/config/routes.rb
 index 5598313..ffb03e7 100644
 --- a/config/routes.rb
 +++ b/config/routes.rb
-@@ -2,6 +2,19 @@ AgileCms::Application.routes.draw do
+@@ -2,6 +2,19 @@ Shiny::Application.routes.draw do
    # The priority is based upon order of creation: first created -> highest priority.
    # See how all your routes lay out with "rake routes".
  
