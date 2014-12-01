685d6571abc136bca0482e0e0be112b923793ee3
Mack Earnhardt <mack@springbuk.com>
2014-12-01 14:52:19 -0500

    Adjustments for Rails 4.2.0.rc1

--- resources/patch/config/environments/production.rb
+++ resources/patch/config/environments/production.rb
@@ -1,5 +1,5 @@
 diff --git a/config/environments/production.rb b/config/environments/production.rb
-index 7c01713..4f163e9 100644
+index 7e47f62..900c049 100644
 --- a/config/environments/production.rb
 +++ b/config/environments/production.rb
 @@ -41,7 +41,7 @@ Rails.application.configure do
@@ -9,9 +9,9 @@ index 7c01713..4f163e9 100644
 -  # config.force_ssl = true
 +  config.force_ssl = true
  
-   # Decrease the log volume.
-   # config.log_level = :info
-@@ -73,5 +73,10 @@ Rails.application.configure do
+   # Use the lowest log level to ensure availability of diagnostic information
+   # when problems arise.
+@@ -74,5 +74,10 @@ Rails.application.configure do
    config.log_formatter = ::Logger::Formatter.new
  
    # Do not dump schema after migrations.
