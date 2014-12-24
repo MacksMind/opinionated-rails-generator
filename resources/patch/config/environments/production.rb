e3e349ec0e21418641d1e4367ded7ea6699f8f6e
Mack Earnhardt <mack@agilereasoning.com>
2014-12-24 09:38:31 -0500

    Tweaks for Rails 4.2.0

--- resources/patch/config/environments/production.rb
+++ resources/patch/config/environments/production.rb
@@ -1,17 +1,17 @@
 diff --git a/config/environments/production.rb b/config/environments/production.rb
-index 7c01713..4f163e9 100644
+index 5c1b32e..1f796ea 100644
 --- a/config/environments/production.rb
 +++ b/config/environments/production.rb
-@@ -41,7 +41,7 @@ Rails.application.configure do
+@@ -42,7 +42,7 @@ Rails.application.configure do
    # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
  
    # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
 -  # config.force_ssl = true
 +  config.force_ssl = true
  
-   # Decrease the log volume.
-   # config.log_level = :info
-@@ -73,5 +73,10 @@ Rails.application.configure do
+   # Use the lowest log level to ensure availability of diagnostic information
+   # when problems arise.
+@@ -75,5 +75,10 @@ Rails.application.configure do
    config.log_formatter = ::Logger::Formatter.new
  
    # Do not dump schema after migrations.
