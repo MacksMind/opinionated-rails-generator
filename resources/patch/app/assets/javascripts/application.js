0d888200273a0b64df44bdab66e3fd78c64ddd27
Mack Earnhardt <mack@agilereasoning.com>
2015-04-03 06:12:13 -0400

    Move jQuery and bootstrap.js back to application.js

--- resources/patch/app/assets/javascripts/application.js
+++ resources/patch/app/assets/javascripts/application.js
@@ -1,12 +1,10 @@
 diff --git a/app/assets/javascripts/application.js b/app/assets/javascripts/application.js
-index d6925fa..9497e44 100644
+index e07c5a8..7465856 100644
 --- a/app/assets/javascripts/application.js
 +++ b/app/assets/javascripts/application.js
-@@ -10,7 +10,6 @@
- // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
- // about supported directives.
- //
--//= require jquery
+@@ -13,4 +13,5 @@
+ //= require jquery
  //= require jquery_ujs
  //= require turbolinks
++//= require bootstrap-sprockets
  //= require_tree .
