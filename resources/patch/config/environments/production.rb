1118b40f9e412aab7e0816cf9102c85c0322f053
Mack Earnhardt <mack@agilereasoning.com>
2017-11-13 07:27:32 -0500

    Add commented settings for SendGrid

--- resources/patch/config/environments/production.rb
+++ resources/patch/config/environments/production.rb
@@ -1,5 +1,5 @@
 diff --git a/config/environments/production.rb b/config/environments/production.rb
-index 2bcc21a..da135c9 100644
+index 2bcc21a..836c25b 100644
 --- a/config/environments/production.rb
 +++ b/config/environments/production.rb
 @@ -45,7 +45,7 @@ Rails.application.configure do
@@ -11,7 +11,7 @@ index 2bcc21a..da135c9 100644
  
    # Use the lowest log level to ensure availability of diagnostic information
    # when problems arise.
-@@ -88,4 +88,9 @@ Rails.application.configure do
+@@ -88,4 +88,20 @@ Rails.application.configure do
  
    # Do not dump schema after migrations.
    config.active_record.dump_schema_after_migration = false
@@ -21,3 +21,14 @@ index 2bcc21a..da135c9 100644
 +
 +  config.action_mailer.default_url_options = { host: config.canonical_hostname, protocol: 'https' }
  end
++
++# Heroku
++# ActionMailer::Base.smtp_settings = {
++#   address:              'smtp.sendgrid.net',
++#   port:                 '587',
++#   authentication:       :plain,
++#   user_name:            ENV['SENDGRID_USERNAME'],
++#   password:             ENV['SENDGRID_PASSWORD'],
++#   domain:               'heroku.com',
++#   enable_starttls_auto: true
++# }
