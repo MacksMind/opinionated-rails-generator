97d3e396e9ba356c51aa1d583ba938f1f1130e20
Mack Earnhardt <mack@agilereasoning.com>
2017-11-13 08:19:54 -0500

    Add require for pundit tests

--- resources/patch/spec/rails_helper.rb
+++ resources/patch/spec/rails_helper.rb
@@ -1,13 +1,14 @@
 diff --git a/spec/rails_helper.rb b/spec/rails_helper.rb
-index bbe1ba5..b300748 100644
+index bbe1ba5..abae20c 100644
 --- a/spec/rails_helper.rb
 +++ b/spec/rails_helper.rb
-@@ -1,3 +1,4 @@
+@@ -1,3 +1,5 @@
 +require 'simplecov'
++require 'pundit/rspec'
  # This file is copied to spec/ when you run 'rails generate rspec:install'
  require 'spec_helper'
  ENV['RAILS_ENV'] ||= 'test'
-@@ -20,7 +21,7 @@ require 'rspec/rails'
+@@ -20,7 +22,7 @@ require 'rspec/rails'
  # directory. Alternatively, in the individual `*_spec.rb` files, manually
  # require only the support files necessary.
  #
@@ -16,7 +17,7 @@ index bbe1ba5..b300748 100644
  
  # Checks for pending migrations and applies them before tests are run.
  # If you are not using ActiveRecord, you can remove this line.
-@@ -29,6 +30,7 @@ ActiveRecord::Migration.maintain_test_schema!
+@@ -29,6 +31,7 @@ ActiveRecord::Migration.maintain_test_schema!
  RSpec.configure do |config|
    # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
    config.fixture_path = "#{::Rails.root}/spec/fixtures"
