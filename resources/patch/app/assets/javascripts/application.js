51e6f93ea7d07a043341a823b26796eb80f276c8
Mack Earnhardt <mack@agilereasoning.com>
2017-11-18 21:23:11 -0500

    Switch to Bootstrap 4 beta

--- resources/patch/app/assets/javascripts/application.js
+++ resources/patch/app/assets/javascripts/application.js
@@ -1,8 +1,8 @@
 diff --git a/app/assets/javascripts/application.js b/app/assets/javascripts/application.js
-index 46b2035..ca1eed0 100644
+index 46b2035..de46d10 100644
 --- a/app/assets/javascripts/application.js
 +++ b/app/assets/javascripts/application.js
-@@ -10,6 +10,8 @@
+@@ -10,6 +10,9 @@
  // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
  // about supported directives.
  //
@@ -10,5 +10,6 @@ index 46b2035..ca1eed0 100644
 +//= require jquery
 +//= require jquery_ujs
  //= require turbolinks
++//= require popper
 +//= require bootstrap-sprockets
  //= require_tree .
--- resources/patch/config/environments/development.rb
+++ resources/patch/config/environments/development.rb
@@ -2,15 +2,6 @@ diff --git a/config/environments/development.rb b/config/environments/developmen
 index 5187e22..a98a88d 100644
 --- a/config/environments/development.rb
 +++ b/config/environments/development.rb
-@@ -40,7 +40,7 @@ Rails.application.configure do
-   # Debug mode disables concatenation and preprocessing of assets.
-   # This option may cause significant delays in view rendering with a large
-   # number of complex assets.
--  config.assets.debug = true
-+  config.assets.debug = false
- 
-   # Suppress logger output for asset requests.
-   config.assets.quiet = true
 @@ -51,4 +51,6 @@ Rails.application.configure do
    # Use an evented file watcher to asynchronously detect changes in source code,
    # routes, locales, etc. This feature depends on the listen gem.
