dfd21d8580ebc1c0dd6a4e9afa82162b97c7f364
Mack Earnhardt <mack@agilereasoning.com>
2018-01-02 12:22:58 -0500

    Move factories_spec.rb so it runs every time

--- resources/patch2/spec/rails_helper.rb
+++ resources/patch2/spec/rails_helper.rb
@@ -8,15 +8,6 @@ index bbe1ba5..abae20c 100644
  # This file is copied to spec/ when you run 'rails generate rspec:install'
  require 'spec_helper'
  ENV['RAILS_ENV'] ||= 'test'
-@@ -20,7 +22,7 @@ require 'rspec/rails'
- # directory. Alternatively, in the individual `*_spec.rb` files, manually
- # require only the support files necessary.
- #
--# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
-+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
- 
- # Checks for pending migrations and applies them before tests are run.
- # If you are not using ActiveRecord, you can remove this line.
 @@ -29,6 +31,7 @@ ActiveRecord::Migration.maintain_test_schema!
  RSpec.configure do |config|
    # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
@@ -25,3 +16,10 @@ index bbe1ba5..abae20c 100644
  
    # If you're not using ActiveRecord, or you'd prefer not to run each of your
    # examples within a transaction, remove the following line or assign false
+@@ -55,3 +55,6 @@ RSpec.configure do |config|
+   # arbitrary gems may also be filtered via:
+   # config.filter_gems_from_backtrace("gem name")
+ end
++
++# Require support files after config because factories_spec.rb needs fixtures
++Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
